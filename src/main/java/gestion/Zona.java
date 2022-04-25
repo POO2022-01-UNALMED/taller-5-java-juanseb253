eaceed66028bd139a9b3a6b697e07ddc0fbdbaa9
juanseb253 <92771458+juanseb253@users.noreply.github.com>
2022-04-25 16:38:20 -0500

    Add files via upload

--- src/main/java/gestion/Zona.java
+++ src/main/java/gestion/Zona.java
@@ -20,7 +20,7 @@ public class Zona {
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	public Zoologico getZoologico() {
+	public Zoologico getZoo() {
 		return zoo;
 	}
 	public void setZoo(Zoologico zoo) {
